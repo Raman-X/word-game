90e2df6d0d56c62eeac7e675023852a94defba33
raman <ramanmaharzan123@gmail.com>
2025-04-27 23:23:20 +0545

    added guess results from guess input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header.jsx";
+import Game from "./components/Game.jsx";
 
 const App = () => {
   return (
-    <div>
+    <div className="wrapper">
       <Header />
+      <div className="game-wrapper">
+        <Game />
+      </div>
     </div>
   );
 };
---
+++ src/components/Game.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { WORDS } from "../data.js";
+
+import GuessInput from "./GuessInput.jsx";
+import GuessResults from "./GuessResults";
+
+// Pick a random word on every pageload.
+const answer = WORDS[Math.floor(Math.random() * WORDS.length)];
+
+console.info({ answer });
+
+function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess]);
+  }
+
+  return (
+    <>
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
+    </>
+  );
+}
+
+export default Game;
