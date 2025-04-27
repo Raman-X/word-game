f696d742d919df9f4545d1a17f69ba9ffa623400
raman <ramanmaharzan123@gmail.com>
2025-04-28 01:59:15 +0545

    keyboard added for visual aid

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,12 +7,13 @@ import GuessResults from "./GuessResults";
 
 import WonBanner from "./WonBanner";
 import LostBanner from "./LostBanner";
-// Pick a random word on every pageload.
-const answer = WORDS[Math.floor(Math.random() * WORDS.length)];
 
-console.info({ answer });
-
+import { checkGuess } from "../checkGuess.js";
+import Keyboard from "./Keyboard";
 function Game() {
+  const [answer, setAnswer] = React.useState(
+    () => WORDS[Math.floor(Math.random() * WORDS.length)],
+  );
   const [guesses, setGuesses] = React.useState([]);
   const [gameStatus, setGameStatus] = React.useState("running");
 
@@ -27,15 +28,32 @@ function Game() {
     }
   }
 
+  function handleRestart() {
+    const newAnswer = WORDS[Math.floor(Math.random() * WORDS.length)];
+    setAnswer(newAnswer);
+    setGuesses([]);
+    setGameStatus("running");
+  }
+
+  const validatedGuesses = guesses.map((guess) => checkGuess(guess, answer));
+
   return (
     <>
-      <GuessResults guesses={guesses} answer={answer} />
+      <GuessResults validatedGuesses={validatedGuesses} />
       <GuessInput
         gameStatus={gameStatus}
         handleSubmitGuess={handleSubmitGuess}
       />
-      {gameStatus === "won" && <WonBanner numOfGuesses={guesses.length} />}
-      {gameStatus === "lost" && <LostBanner answer={answer} />}
+      <Keyboard validatedGuesses={validatedGuesses} />
+      {gameStatus === "won" && (
+        <WonBanner
+          numOfGuesses={guesses.length}
+          handleRestart={handleRestart}
+        />
+      )}
+      {gameStatus === "lost" && (
+        <LostBanner answer={answer} handleRestart={handleRestart} />
+      )}
     </>
   );
 }
--- src/components/GuessResults.jsx
+++ src/components/GuessResults.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import Guess from "./Guess";
 import range from "../range.js";
 
-function GuessResults({ guesses, answer }) {
+function GuessResults({ validatedGuesses }) {
   return (
     <div className="guess-results">
       {range(6).map((num) => (
-        <Guess key={num} value={guesses[num]} answer={answer} />
+        <Guess key={num} value={validatedGuesses[num]} />
       ))}
     </div>
   );
